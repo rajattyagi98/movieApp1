b759614edf997fc062ae1ed6f3778c6a0a892d6c
Rajat Tyagi <43870624+rajattyagi98@users.noreply.github.com>
2023-10-28 11:37:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+app.get("/movies", (req, res) =>{
+	res.send("All Movies Data in JSON format from Mongo DB");
+})
+app.get("/genres", (req, res) =>{
+	res.send("All Genres Data in JSON format from Mongo DB");
+})
+app.get("/artists", (req, res) =>{
+	res.send("All Artists Data in JSON format from Mongo DB");
+})
+app.listen(9000, (req, res) => {
+	console.log('Server listening ....');
+})
